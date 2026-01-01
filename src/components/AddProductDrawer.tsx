d5d748a89fa46c01ad738654f185364e238ca75b
roymichaels <RoyMichaelsDotCom@pm.me>
2026-01-01 06:41:20 -0600

    Deleted RoutePlanner.tsx

--- src/components/AddProductDrawer.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// Placeholder to satisfy legacy imports; real implementation should live elsewhere.
-export default function AddProductDrawer(props: any) {
-  return <div {...props} />;
-}
